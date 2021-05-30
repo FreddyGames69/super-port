e0726fcbadd4c08a11f82ec1c08b726ab5d62b00
freddy <freddy12gutierrez@gmail.com>
2021-05-31 01:43:50 +0200

    resumen añadido beta1

--- custom.js
+++ custom.js
@@ -15,6 +15,8 @@ const abrirHome = () =>{
         main.classList.remove("show-main");
         let nav = document.querySelector(".header-main");
         nav.classList.remove("show-nav");
+        let resume = document.querySelector(".resume");
+        resume.classList.remove("show-resume");
     });
 }
 abrirHome();
@@ -27,6 +29,22 @@ const abrirAbout = () => {
         main.classList.add("show-main");
         let nav = document.querySelector(".header-main");
         nav.classList.remove("show-nav");
+        let resume = document.querySelector(".resume");
+        resume.classList.remove("show-resume");
     });
 }
 abrirAbout();
+
+
+const abrirResume = () => {
+    let boton = document.querySelector("#btn-resume");
+    boton.addEventListener('click', ()=>{
+        let resume = document.querySelector(".resume");
+        resume.classList.add("show-resume");
+        let nav = document.querySelector(".header-main");
+        nav.classList.remove("show-nav");
+        let main = document.querySelector(".main");
+        main.classList.remove("show-main");
+    });
+}
+abrirResume();
