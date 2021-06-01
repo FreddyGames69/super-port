df9f072ef903092898348b7fcf52a0ea3ccd0c08
freddy <freddy12gutierrez@gmail.com>
2021-06-01 07:35:05 -0600

    se añadio animate css, se añadio el area de portafolio, y algunas mejoran en el js custom

--- custom.js
+++ custom.js
@@ -1,3 +1,15 @@
+//BOTONES
+const botonHome = document.querySelector("#btn-home");
+const botonAbout = document.querySelector("#btn-about");
+const botonResume = document.querySelector("#btn-resume");
+const botonPortafolio = document.querySelector("#btn-portfolio");
+//SECCIONES
+const main = document.querySelector(".main");
+const nav = document.querySelector(".header-main");
+const resume = document.querySelector(".resume");
+const port = document.querySelector(".portfolio");
+
+
 const abrirMenuMobile = () =>{
     let boton = document.querySelector("#btn-menu-mobile");
     boton.addEventListener('click', ()=>{
@@ -9,42 +21,63 @@ abrirMenuMobile();
 
 
 const abrirHome = () =>{
-    let boton = document.querySelector("#btn-home");
-    boton.addEventListener('click', ()=>{
-        let main = document.querySelector(".main");
+    botonHome.addEventListener('click', ()=>{
         main.classList.remove("show-main");
-        let nav = document.querySelector(".header-main");
         nav.classList.remove("show-nav");
-        let resume = document.querySelector(".resume");
         resume.classList.remove("show-resume");
+        port.classList.remove("show-portfolio");
+        botonHome.classList.add('ico-active');
+        botonAbout.classList.remove('ico-active');
+        botonResume.classList.remove('ico-active');
+        botonPortafolio.classList.remove('ico-active');
     });
 }
 abrirHome();
 
 
 const abrirAbout = () => {
-    let boton = document.querySelector("#btn-about");
-    boton.addEventListener('click', ()=>{
-        let main = document.querySelector(".main");
+    botonAbout.addEventListener('click', ()=>{
         main.classList.add("show-main");
-        let nav = document.querySelector(".header-main");
+        main.classList.add("animate__bounceInLeft");
         nav.classList.remove("show-nav");
-        let resume = document.querySelector(".resume");
         resume.classList.remove("show-resume");
+        port.classList.remove("show-portfolio");
+        botonAbout.classList.add('ico-active');
+        botonHome.classList.remove('ico-active');
+        botonResume.classList.remove('ico-active');
+        botonPortafolio.classList.remove('ico-active');
     });
 }
 abrirAbout();
 
 
 const abrirResume = () => {
-    let boton = document.querySelector("#btn-resume");
-    boton.addEventListener('click', ()=>{
-        let resume = document.querySelector(".resume");
+    botonResume.addEventListener('click', ()=>{
         resume.classList.add("show-resume");
-        let nav = document.querySelector(".header-main");
+        resume.classList.add("animate__fadeInTopLeft")
         nav.classList.remove("show-nav");
-        let main = document.querySelector(".main");
         main.classList.remove("show-main");
+        port.classList.remove("show-portfolio");
+        botonResume.classList.add('ico-active');
+        botonAbout.classList.remove('ico-active');
+        botonHome.classList.remove('ico-active');
+        botonPortafolio.classList.remove('ico-active');
     });
 }
 abrirResume();
+
+
+const abrirPortfolio = () => {
+    botonPortafolio.addEventListener('click', ()=>{
+        port.classList.add("show-portfolio");
+        port.classList.add("animate__bounceInLeft")
+        nav.classList.remove("show-nav");
+        main.classList.remove("show-main");
+        resume.classList.remove("show-resume");
+        botonPortafolio.classList.add('ico-active');
+        botonResume.classList.remove('ico-active');
+        botonAbout.classList.remove('ico-active');
+        botonHome.classList.remove('ico-active');
+    });
+}
+abrirPortfolio();
