60cb7153995556dab43ccccb124855081df6e24c
freddy <freddy12gutierrez@gmail.com>
2021-06-02 03:09:21 -0600

    version mobile 1 terminada

--- custom.js
+++ custom.js
@@ -3,11 +3,13 @@ const botonHome = document.querySelector("#btn-home");
 const botonAbout = document.querySelector("#btn-about");
 const botonResume = document.querySelector("#btn-resume");
 const botonPortafolio = document.querySelector("#btn-portfolio");
+const botonContact = document.querySelector("#btn-contact");
 //SECCIONES
 const main = document.querySelector(".main");
 const nav = document.querySelector(".header-main");
 const resume = document.querySelector(".resume");
 const port = document.querySelector(".portfolio");
+const contact = document.querySelector(".contact");
 
 
 const abrirMenuMobile = () =>{
@@ -26,10 +28,12 @@ const abrirHome = () =>{
         nav.classList.remove("show-nav");
         resume.classList.remove("show-resume");
         port.classList.remove("show-portfolio");
+        contact.classList.remove("show-contac");
         botonHome.classList.add('ico-active');
         botonAbout.classList.remove('ico-active');
         botonResume.classList.remove('ico-active');
         botonPortafolio.classList.remove('ico-active');
+        botonContact.classList.remove('ico-active');
     });
 }
 abrirHome();
@@ -42,10 +46,12 @@ const abrirAbout = () => {
         nav.classList.remove("show-nav");
         resume.classList.remove("show-resume");
         port.classList.remove("show-portfolio");
+        contact.classList.remove("show-contac");
         botonAbout.classList.add('ico-active');
         botonHome.classList.remove('ico-active');
         botonResume.classList.remove('ico-active');
         botonPortafolio.classList.remove('ico-active');
+        botonContact.classList.remove('ico-active');
     });
 }
 abrirAbout();
@@ -54,14 +60,16 @@ abrirAbout();
 const abrirResume = () => {
     botonResume.addEventListener('click', ()=>{
         resume.classList.add("show-resume");
-        resume.classList.add("animate__fadeInTopLeft")
+        resume.classList.add("animate__fadeInTopLeft");
         nav.classList.remove("show-nav");
         main.classList.remove("show-main");
         port.classList.remove("show-portfolio");
+        contact.classList.remove("show-contac");
         botonResume.classList.add('ico-active');
         botonAbout.classList.remove('ico-active');
         botonHome.classList.remove('ico-active');
         botonPortafolio.classList.remove('ico-active');
+        botonContact.classList.remove('ico-active');
     });
 }
 abrirResume();
@@ -70,14 +78,33 @@ abrirResume();
 const abrirPortfolio = () => {
     botonPortafolio.addEventListener('click', ()=>{
         port.classList.add("show-portfolio");
-        port.classList.add("animate__bounceInLeft")
+        port.classList.add("animate__bounceInLeft");
         nav.classList.remove("show-nav");
         main.classList.remove("show-main");
         resume.classList.remove("show-resume");
+        contact.classList.remove("show-contac");
         botonPortafolio.classList.add('ico-active');
         botonResume.classList.remove('ico-active');
         botonAbout.classList.remove('ico-active');
         botonHome.classList.remove('ico-active');
+        botonContact.classList.remove('ico-active');
     });
 }
 abrirPortfolio();
+
+const abrirContact = () => {
+    botonContact.addEventListener('click', ()=>{
+        contact.classList.add("show-contac");
+        contact.classList.add("animate__fadeInTopLeft")
+        port.classList.remove("show-portfolio");
+        nav.classList.remove("show-nav");
+        main.classList.remove("show-main");
+        resume.classList.remove("show-resume");
+        botonPortafolio.classList.remove('ico-active');
+        botonResume.classList.remove('ico-active');
+        botonAbout.classList.remove('ico-active');
+        botonHome.classList.remove('ico-active');
+        botonContact.classList.add('ico-active');
+    });
+}
+abrirContact();
